d74ee24cda068a7175524acccc687b0fd7b299ae
jsaikamal <138606431+jsaikamal@users.noreply.github.com>
2024-02-03 12:15:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ window.addEventListener("scroll",() =>{
     }
 })
 
+      const countEl = document.getElementById("count");
+      countvisits();
+
+      function countvisits() {
+        fetch('https://api.countapi.xyz/update/laptop/mouse/?amount=1')
+          .then((res) => res.json())
+          .then((res) => {
+            countEl.innerHTML = res.value;
+          });
+      }
