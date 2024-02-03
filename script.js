dc0b0a035ece878c1db570b613f490400741a7a6
jsaikamal <138606431+jsaikamal@users.noreply.github.com>
2024-02-03 12:43:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,13 +18,15 @@ window.addEventListener("scroll",() =>{
     }
 })
 
-      const countEl = document.getElementById("count");
-      countvisits();
 
-      function countvisits() {
-        fetch('https://api.countapi.xyz/update/laptop/mouse/?amount=1')
-          .then((res) => res.json())
-          .then((res) => {
-            countEl.innerHTML = res.value;
-          });
-      }
+function updateCounter() {
+    fetch('https://api.countapi.xyz/update/uimonk/youtubechannel/?amount=1')
+        .then(res => res.json())
+        .then(data => counterElement.innerHTML = data.value)
+}
+
+updateCounter()
+
+
+
+counterElement = document.getElementsByClassName('count')[0];
